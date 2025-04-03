b3091160e080b8d0b853516478e0830415327f70
Oleksandr Kuzmenko <f3dstudiya@gmail.com>
2025-04-03 16:22:54 +0100

    Added proxy method for showing single product

--- src/Controller/ApiGatewayController.php
+++ src/Controller/ApiGatewayController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class ApiGatewayController extends AbstractController
@@ -26,5 +29,29 @@ final class ApiGatewayController extends AbstractController
             $response->getStatusCode(),
             ['Content-Type' => $response->getHeaders()['content-type'][0]]
         );
+    }
+
+    #[Route('/api/product/{id}', name: 'app_api_gateway_product', methods: ['GET'])]
+    public function proxyProduct(string $id): Response
+    {
+        $url = $_ENV['PRODUCT_SVC_HOST'] . '/product/' . urlencode($id);
+
+        try {
+            $response = $this->httpClient->request('GET', $url, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+            ]);
+
+            return new Response(
+                $response->getContent(),
+                $response->getStatusCode(),
+                ['Content-Type' => $response->getHeaders()['content-type'][0]]
+            );
+        } catch (ClientExceptionInterface $e) {
+            return new JsonResponse(['error' => 'Product not found'], 404);
+        } catch (Exception $e) {
+            return new JsonResponse(['error' => 'Internal server error'], 500);
+        }
     }
 }
